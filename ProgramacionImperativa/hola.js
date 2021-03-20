a1abb6892c2e4b6469dfa0b73eb25387dea35fc5
BrendaNolan <brendacnolan@gmail.com>
2021-03-20 15:12:44 -0300

    cree la carpeta ejercicioCommit

---
+++ ProgramacionImperativa/hola.js
@@ -0,0 +1,29 @@
+console.log ("hola mundo")
+
+
+let nombre = "brenda";
+let edad = 27;
+let esAbogada = true;
+console.log (nombre);
+console.log (edad);
+console.log (esAbogada);
+console.log (5+4);
+console.log (5-4)
+let numeroUno = 1;
+let numeroDos = 2
+console.log (numeroUno + numeroDos);
+let numeroTres = numeroUno + numeroDos;
+console.log (numeroTres)
+numeroTres = "1"+1;
+console.log (numeroTres);
+let nombre1 = "Nicolas";
+let apellido1 = "Lopez";
+let edad1 = 27;
+let peso1 = 83.5;
+let altura1 = 1.70 ;
+let prepaga1 = true ;
+let IMC1 = peso1 / (altura1**2);
+
+console.log("El indice de masa corporal de "+ nombre1 + " es" + IMC1)
+
+
